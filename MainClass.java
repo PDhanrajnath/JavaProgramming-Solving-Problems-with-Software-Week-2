a8ae26760aa3c6a65eb33243c5cd696b1a4c414a
Porika Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2021-06-19 16:34:21 +0530

    first commit

---
+++ MainClass.java
@@ -0,0 +1,10 @@
+import javax.swing.text.html.HTML;
+
+public class MainClass {
+    public static void main(String args[]){
+        String a = "cccatggggtttaaataataataggagagagagagagagttt";
+        String ap = "atggggtttaaataataatag";
+        TagFinder tf=new TagFinder();
+        tf.testing(ap);
+    }
+}
